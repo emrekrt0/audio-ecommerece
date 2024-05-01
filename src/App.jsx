ea97c33dc096139cc45f9ecebb6484bcdc3612b0
Emre Kurt <emre.kurt1099@gmail.com>
2024-05-01 17:13:49 +0300

    Headphones kategorisi yapildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Container from './components/Content'
+import Footer from './components/Footer'
+import { Outlet, useNavigate, useLocation } from 'react-router-dom'
 
 function App() {
+  const navigate = useNavigate()
+  const location = useLocation()
 
+  useEffect(() => {
+   location.pathname === '/' ? navigate('/home') : null;
+}, [location, navigate]);
   return (
     <>
       <Header />
-      <Container />
+      <Outlet />
+      <Footer />
     </>  
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Content from './components/Content.jsx'
+import Headphones from './components/Headphones.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        path: '/home',
+        element: <Content />,
+      },
+      {
+        path: '/headphones',
+        element: <Headphones />,
+      },
+      {
+        path: '/speakers',
+        element: <App />,
+      },
+      {
+        path: '/earphones',
+        element: <App />,
+      },
+    ],
   }
 ])
 
