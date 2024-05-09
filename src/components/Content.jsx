c7b6c2f990d1c845e55f99d62d03c64e3cfca111
Emre Kurt <emre.kurt1099@gmail.com>
2024-05-10 02:35:45 +0300

    kucuk duzeltme

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,6 +6,7 @@ import bigSpeaker from '../assets/bigProductSelection/bigSpeaker.png';
 import Group6 from '../assets/productselection/Group6.svg';
 import zx7speaker from '../assets/carts/zx7speaker.svg';
 import yx1earphones from '../assets/carts/yx1earphones.svg';
+import FirstComp from './smallComp/FirstComp';
 
 export function MainPageHeader() {
     return (
@@ -30,68 +31,7 @@ export default function Container() {
             <>
             
             <div className="containerWrapper">
-                <div className="selectionContent">
-                    <div className="selectionProduct">
-                        <div className="selectionSVG">
-                            <svg width="327" height="165" viewBox="0 0 327 165" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <rect width="327" height="165" rx="8" fill="#F1F1F1"/>
-                            </svg>
-                        </div>
-                        <div className='selectionImages'>
-                            <img src={headphoneImage} alt="" />
-                            <img src={oval} alt="" className='ovalPlace'/>
-                        </div>
-                        <div className="selectionTexts">
-                            <h5 className='selectionHeaders'>HEADPHONES</h5>
-                            <div className="button3">
-                                <h5 className='mButton3'>SHOP</h5>
-                                <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path   d="M1.32178 1L6.32178 6L1.32178 11" stroke="#D87D4A" stroke-width="2"/>
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="selectionProduct">
-                        <div className="selectionSVG">
-                            <svg width="327" height="165" viewBox="0 0 327 165" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <rect width="327" height="165" rx="8" fill="#F1F1F1"/>
-                            </svg>
-                        </div>
-                        <div className='selectionImages'>
-                            <img src={speakerImage} alt="" />
-                            <img src={oval} alt="" className='ovalPlace'/>
-                        </div>
-                        <div className="selectionTexts">
-                            <h5 className='selectionHeaders'>SPEAKERS</h5>
-                            <div className="button3">
-                                <h5 className='mButton3'>SHOP</h5>
-                                <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path   d="M1.32178 1L6.32178 6L1.32178 11" stroke="#D87D4A" stroke-width="2"/>
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="selectionProduct">
-                        <div className="selectionSVG">
-                            <svg width="327" height="165" viewBox="0 0 327 165" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <rect width="327" height="165" rx="8" fill="#F1F1F1"/>
-                            </svg>
-                        </div>
-                        <div className='selectionImages'>
-                            <img src={earphoneImage} alt="" />
-                            <img src={oval} alt="" className='ovalPlace'/>
-                        </div>
-                        <div className="selectionTexts">
-                            <h5 className='selectionHeaders'>EARPHONES</h5>
-                            <div className="button3">
-                                <h5 className='mButton3'>SHOP</h5>
-                                <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path   d="M1.32178 1L6.32178 6L1.32178 11" stroke="#D87D4A" stroke-width="2"/>
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <FirstComp />
                 <div className="bigProduct">
                     <div className="bigSpeaker">
                         <div className="bigSpeakerImg">
--- src/components/smallComp/FirstComp.jsx
+++ src/components/smallComp/FirstComp.jsx
@@ -10,6 +10,14 @@ import { hMenuContext } from '../../App';
 export default function FirstComp() {
     const {hamMenu, setHamMenu} = useContext(hMenuContext)
 
+    function scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
+
     function toggleHamMenu() {
         setHamMenu(false)
         console.log(hamMenu, 'hamMenu firstcomp');
@@ -29,7 +37,7 @@ export default function FirstComp() {
                         <div className="selectionTexts">
                             <h5 className='selectionHeaders'>HEADPHONES</h5>
                             <div className="button3">
-                                <NavLink to={'/headphones'} onClick={toggleHamMenu}><h5 className='mButton3'>SHOP</h5></NavLink>
+                                <NavLink to={'/headphones'} onClick={() => {toggleHamMenu(); scrollToTop()}}><h5 className='mButton3'>SHOP</h5></NavLink>
                                 <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path   d="M1.32178 1L6.32178 6L1.32178 11" stroke="#D87D4A" stroke-width="2"/>
                                 </svg>
@@ -49,7 +57,7 @@ export default function FirstComp() {
                         <div className="selectionTexts">
                             <h5 className='selectionHeaders'>SPEAKERS</h5>
                             <div className="button3">
-                            <NavLink to={'/speakers'} onClick={toggleHamMenu}><h5 className='mButton3'>SHOP</h5></NavLink>
+                            <NavLink to={'/speakers'} onClick={() => {toggleHamMenu(); scrollToTop()}}><h5 className='mButton3'>SHOP</h5></NavLink>
                                 <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path   d="M1.32178 1L6.32178 6L1.32178 11" stroke="#D87D4A" stroke-width="2"/>
                                 </svg>
@@ -69,7 +77,7 @@ export default function FirstComp() {
                         <div className="selectionTexts">
                             <h5 className='selectionHeaders'>EARPHONES</h5>
                             <div className="button3">
-                            <NavLink to={'/earphones'} onClick={toggleHamMenu}><h5 className='mButton3'>SHOP</h5></NavLink>
+                            <NavLink to={'/earphones'} onClick={() => {toggleHamMenu(); scrollToTop()}}><h5 className='mButton3'>SHOP</h5></NavLink>
                                 <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path   d="M1.32178 1L6.32178 6L1.32178 11" stroke="#D87D4A" stroke-width="2"/>
                                 </svg>
