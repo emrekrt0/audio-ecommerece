8fb9ec79344ef04043763b34b5d46365fc97d088
Emre Kurt <emre.kurt1099@gmail.com>
2024-05-23 18:09:19 +0300

    Detay sayfasi ve urun oneri sayfasi tamamiyle supabase baglandi

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,23 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import sp1 from '../assets/categoryImgs/speakers/ss1.png'
-import sp2 from '../assets/categoryImgs/speakers/ss2.png'
-import spd11 from '../assets/detailImgs/speakers/spd11.jpg'
-import spd12 from '../assets/detailImgs/speakers/spd12.jpg'
-import spd13 from '../assets/detailImgs/speakers/spd13.jpg'
-import spd21 from '../assets/detailImgs/speakers/spd21.jpg'
-import spd22 from '../assets/detailImgs/speakers/spd22.jpg'
-import spd23 from '../assets/detailImgs/speakers/spd23.jpg'
-import ep1 from '../assets/categoryImgs/earphones/ep1.png'
-import epd11 from '../assets/detailImgs/earphones/epd11.jpg'
-import epd12 from '../assets/detailImgs/earphones/epd12.jpg'
-import epd13 from '../assets/detailImgs/earphones/epd13.jpg'
-
 import FirstComp from "./smallComp/FirstComp";
 import YouMayAlsoLike from "./smallComp/YouMayAlsoLike";
 import {useNavigate} from 'react-router-dom';
+import supabase from "./smallComp/Supabase";
 
 export default function ProductDetail() {
+    const [product,setProduct] = useState();
     const navigate = useNavigate();
     const { slug } = useParams();
 
@@ -30,85 +19,115 @@ export default function ProductDetail() {
         })
     }
 
-    const product = products.find(p => p.slug === slug);
+    useEffect(() => {
+        getProductDetails();
+    }, [slug]);
+
+    async function getProductDetails(){
+        try{
+        const {data, error} = await supabase
+        .from('products')
+        .select('*')
+        .eq('slug', slug)
+        if (error) {
+            console.log(error);
+        }
+        else{
+            console.log(data);
+            setProduct(data);
+        }
+        }catch(error) {
+            console.log(error);
+        }
+    }
+
     console.log(product);
     return (
         <>
-            <div className="productDetailContainer">
-                <div className="goBckBtn" onClick={handleGoBack}>
-                    <p className="mBody tal">Go Back</p>
-                </div>
-                <div className="productDetail">
-                    <div className="productDetailTop">
-                        <div className="productDetailTopImg">
-                            <div className="bckgrnd">
-                                <svg width="281" height="480"  fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <rect width="281" height="480" rx="8" fill="#F1F1F1"/>
-                                </svg>
-                            </div>
-                            <img src={product.image} alt="" />
-                        </div>
-                        <div className="productDetail-Texts-Cart">
-                            <div className="productDetailTexts">
-                                {product.new && <div className="newProduct mOverline txtOrange"><p>NEW PRODUCT</p></div>}
-                                <div className="productHeadline">
-                                    <h4 className="mH4 ls-1">{product.title} {product.category}</h4>
-                                </div>
-                                <div className="productText">
-                                    <p className="mBody tal">{product.description}</p>
-                                </div>
-                                <div className="productPrice">
-                                    <h6 className="mH6">{product.price}</h6>
-                                </div>
-                            </div>
-                        
-                            <div className="productAddToCart">
-                                <div className="productQty w120h48 spcevenly">
-                                    <div className="qtyRmv pQtyNumb op25">
-                                        -
-                                    </div>
-                                    <h3 className='pQtyNumb'>1</h3>
-                                    <div className="qtyAdd pQtyNumb op25">
-                                        +
-                                    </div>
-                                </div>
-                                <div className="productButton">
-                                    <p className="mButton1 w160h48 tac">ADD TO CART</p>
-                                </div>
-                            </div>
-                        </div>
+            {product ? product.map((product) => (
+                <>
+                    <div className="productDetailContainer">
+                    <div className="goBckBtn" onClick={handleGoBack}>
+                        <p className="mBody tal">Go Back</p>
                     </div>
-                        <div className="productFeatures">
-                            <h5 className="featuresH5">FEATURES</h5>
-                            <p className="mBody tal">{product.features_text}</p>
-                            <p className="mBody tal">{product.features_text2}</p>
-                        </div>
-                        <div className="productInTheBox">
-                            <h5 className="featuresH5">IN THE BOX</h5>
-                            <div className="inTheBox">
-                                {product.in_the_box.map((item, index) => (
-                                    <div key={index} className="inTheBoxItem">
-                                        <h5 className="mSubTitle fs15 txtOrange">{item[0]}</h5>
-                                        <p className="mBody tal">{item[1]}</p>
+                    <div className="productDetail">
+                        <div className="productDetailTop">
+                            <div className="productDetailTopImg">
+                                <div className="bckgrnd">
+                                    <svg width="281" height="480"  fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <rect width="281" height="480" rx="8" fill="#F1F1F1"/>
+                                    </svg>
+                                </div>
+                                <img src={product.image} alt="" />
+                            </div>
+                            <div className="productDetail-Texts-Cart">
+                                <div className="productDetailTexts">
+                                    {product.new && <div className="newProduct mOverline txtOrange"><p>NEW PRODUCT</p></div>}
+                                    <div className="productHeadline">
+                                        <h4 className="mH4 ls-1">{product.title} {product.category.en}</h4>
                                     </div>
+                                    <div className="productText">
+                                        <p className="mBody tal">{product.description.en}</p>
+                                    </div>
+                                    <div className="productPrice">
+                                        <h6 className="mH6">{product.price}</h6>
+                                    </div>
+                                </div>
+                            
+                                <div className="productAddToCart">
+                                    <div className="productQty w120h48 spcevenly">
+                                        <div className="qtyRmv pQtyNumb op25">
+                                            -
+                                        </div>
+                                        <h3 className='pQtyNumb'>1</h3>
+                                        <div className="qtyAdd pQtyNumb op25">
+                                            +
+                                        </div>
+                                    </div>
+                                    <div className="productButton">
+                                        <p className="mButton1 w160h48 tac">ADD TO CART</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                            <div className="productFeatures">
+                                <h5 className="featuresH5">FEATURES</h5>
+                                {product.features_text.en.map((feature, index) => (
+                                    <>
+                                    <p className="mBody tal" key={index[0]}>{feature[0]}</p>
+                                    <p className="mBody tal" key={index[1]}>{feature[1]}</p>
+                                    </>
+                                ))}
+                            </div>
+                            <div className="productInTheBox">
+                                <h5 className="featuresH5">IN THE BOX</h5>
+                                <div className="inTheBox">
+                                    {product.in_the_box.en.map((item, index) => (
+                                        <div key={index} className="inTheBoxItem">
+                                            <h5 className="mSubTitle fs15 txtOrange">{item[0]}</h5>
+                                            <p className="mBody tal">{item[1]}</p>
+                                        </div>
+                                    
+                                    ))}
+                                </div>
+                            </div>
+                            <div className="productDetailImg">
+                                {product.detail_images.map((img,index) => (
+                                    <img key={index} src={img} alt="" />
                                 
                                 ))}
                             </div>
                         </div>
-                        <div className="productDetailImg">
-                            {product.detail_images.map((img,index) => (
-                                <img key={index} src={img} alt="" />
-                            
-                            ))}
-                        </div>
                     </div>
-                </div>
-                <div className="youMayAlsoLike">
-                        <YouMayAlsoLike products={products} currentProductSlug={slug} />
-                </div>
-                <div className="containerWrapper">
-                    <FirstComp />
-                </div>
+                    <div className="youMayAlsoLike">
+                        <YouMayAlsoLike category={product.category.en} currentProductSlug={slug} />
+                    </div>
+                    <div className="containerWrapper">
+                        <FirstComp />
+                    </div>
+                </>
+                
+            )) : 'Loading...'}
         </>
     );
 }
--- src/components/smallComp/YouMayAlsoLike.jsx
+++ src/components/smallComp/YouMayAlsoLike.jsx
@@ -1,20 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import scrollToTop from './scrollToTop';
+import supabase from './Supabase';
+import scrollToTop  from './scrollToTop';
 
-const YouMayAlsoLike = ({ products, currentProductSlug }) => {
-    const currentProduct = products.find(product => product.slug === currentProductSlug);
-    const currentCategory = currentProduct.category;
-    const sameCategoryProducts = products.filter(product => product.slug !== currentProductSlug && product.category === currentCategory);
-    const otherProducts = products.filter(product => product.slug !== currentProductSlug && product.category !== currentCategory);
-    const firstThreeProducts = [...sameCategoryProducts, ...otherProducts].slice(0, 3);
+const YouMayAlsoLike = ({ category, currentProductSlug }) => {
+    const [products, setProducts] = useState([]);
 
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data, error } = await supabase
+                .from('products')
+                .select('*')
+                .order('id', { ascending: true })
+                .neq('slug', currentProductSlug)
+                .eq('category->>en', category)
+                .limit(3)
+            
+            if (error) {
+                console.log(error);
+                return;
+            }
+
+            if (data.length < 3) {
+                const { data: data2, error: error2 } = await supabase
+                    .from('products')
+                    .select('*')
+                    .order('id', { ascending: true })
+                    .neq('slug', currentProductSlug)
+                    .neq('category->>en', category)
+                    .limit(3 - data.length);
+                if (error2) {
+                    console.log(error2);
+                } else {
+                    setProducts([...data, ...data2]);
+                }
+            }
+
+            else {
+                setProducts(data);
+            }
+        };
+
+        fetchProducts();
+    }, [category, currentProductSlug]);
+    console.log(products);
     return (
-    <>
-        <h5 className='featuresH5'>YOU MAY ALSO LIKE</h5 >
-        <div className='ymayLikeContainer'>
-            {firstThreeProducts.map(product => (
-                <div className='ymayLikeContent' key={product.slug}>
+        <>
+            <h5 className='featuresH5'>YOU MAY ALSO LIKE</h5 >
+            <div className='ymayLikeContainer'>
+            {products.length > 0 ? (
+                products.map((product) => (
+                    <div className='ymayLikeContent' key={product.slug}>
                     <div className="ymayLikeImg">
                         <div className="grayBG">
                             <svg width="327" height="120" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -24,14 +60,14 @@ const YouMayAlsoLike = ({ products, currentProductSlug }) => {
                         <img src={product.image} alt={product.title} />
                     </div>
                     <h5 className='mH5 tac'>{product.title} {product.title.length <=8 && 
-                    product.title !== 'XX59' && product.category}</h5>
+                    product.title !== 'XX59' && product.category.en}</h5>
                     <div class="contentButton">
                         <Link to={`/details/${product.slug}`} onClick={scrollToTop}><p class="mButton1 tac">SEE PRODUCT</p></Link>
                     </div>
                 </div>
-            ))}
+            ))): 'Loading...'}
         </div>
-    </>
+        </>
     );
 };
 
